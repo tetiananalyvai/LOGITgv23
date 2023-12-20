93793ca2ec5868847a4adc6aced8d304a28e6c28
opilane <opilane@HP-CZC2349HT2>
2023-12-20 17:11:03 +0200

    Add project files.

---
+++ LOGITgv23.py
@@ -0,0 +1,37 @@
+#print("Tere maailm")
+#nimi=input("Mis on sinu nimi?")
+#vanus=int(input("Kui vana sa oled?"))
+
+#print("Tere "+nimi+"! Sa oled "+str(vanus)+" aastat vana.")
+#print("Tere",nimi,"! Sa oled",vanus,"aastat vana.")
+#print("Tere {0}! Sa oled {1} aastat vana".format(nimi,vanus))
+
+#print(type(nimi))
+#print(type(vanus))
+
+#arv1=float(input("Arv 1: "))
+#t=input("Tehe: ")
+#arv2=float(input("Arv 2: "))
+#vastus=eval(str(arv1)+t+str(arv2))
+#print("{0}{1}{2}={3}".format(arv1,t,arv2,vastus))
+
+#vanus = 18
+#eesnimi = "Jaak"
+#pikkus = 16.5
+#kas_käib_koolis = True
+#print("Muutuja",vanus,"on",type(vanus))
+#print("Muutuja",eesnimi,"on",type(eesnimi))
+#print("Muutuja",pikkus,"on",type(pikkus))
+#print("Muutuja",kas_käib_koolis,"on",type(kas_käib_koolis))
+
+#from random import * #kõik funktsioonid mis on moodulis
+#kogus=randint(1,100) #66
+#print("Kokku on",kogus,"kommid")
+#mitu=int(input("Mitu kommi tahad võtta")) #33
+#kogus=kogus-mitu #kogus=kogus-mitu->kogus-=mitu
+#print("Laua peal on",kogus,"kommid")
+
+#from math import *
+#l=float(input("Ümbermõõt:"))
+#d=round(l/pi,2)  
+#print("Läbimõõt=", d)
